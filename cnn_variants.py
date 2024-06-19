d5fd688db0ed62f997b4cbc8c19e4695197dd1e4
omarhassanein8 <123910730+omarhassanein8@users.noreply.github.com>
2024-06-18 23:50:34 -0400

    seperated files

--- cnn_variants.py
+++ cnn_variants.py
@@ -10,6 +10,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from collections import Counter
 from tabulate import tabulate
+from cnn_models import FacialStateCNN, Variant1CNN, Variant2CNN
 
 #preprocess the data
 transform = transforms.Compose([
@@ -45,115 +46,6 @@ train_classes = [label for _, label in train_dataset]
 class_weights = compute_class_weight('balanced', classes=np.unique(train_classes), y=train_classes)
 class_weights_tensor = torch.tensor(class_weights, dtype=torch.float)
 
-#main model with no changes to conv layers and kernel size
-class FacialStateCNN(nn.Module):
-    def __init__(self):
-        super(FacialStateCNN, self).__init__()
-        self.conv_layer = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.fc_layer = nn.Sequential(
-            nn.Dropout(p=0.1),
-            nn.Linear(12 * 12 * 128, 1000),
-            nn.ReLU(inplace=True),
-            nn.Linear(1000, 512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.1),
-            nn.Linear(512, 4)  #4 classes: 0 happy, 1 neutral, 2 focused, 3 angry
-        )
-
-    def forward(self, x):
-        x = self.conv_layer(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc_layer(x)
-        return x
-    
-#variant 1 extra convulotional layers than main model
-class Variant1CNN(nn.Module):
-    def __init__(self):
-        super(Variant1CNN, self).__init__()
-        self.conv_layer = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.fc_layer = nn.Sequential(
-            nn.Dropout(p=0.1),
-            nn.Linear(12 * 12 * 256, 1000),
-            nn.ReLU(inplace=True),
-            nn.Linear(1000, 512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.1),
-            nn.Linear(512, 4)
-        )
-
-    def forward(self, x):
-        x = self.conv_layer(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc_layer(x)
-        return x
-#variant 2 increased kernel size
-class Variant2CNN(nn.Module):
-    def __init__(self):
-        super(Variant2CNN, self).__init__()
-        self.conv_layer = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, padding=2),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=2),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, padding=2),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=5, padding=2),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.fc_layer = nn.Sequential(
-            nn.Dropout(p=0.1),
-            nn.Linear(12 * 12 * 128, 1000),
-            nn.ReLU(inplace=True),
-            nn.Linear(1000, 512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.1),
-            nn.Linear(512, 4)
-        )
-
-    def forward(self, x):
-        x = self.conv_layer(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc_layer(x)
-        return x
 
 #training function
 def train_model(model, train_loader, val_loader,  num_epochs=50):
